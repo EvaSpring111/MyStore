4e992dfcebcae4a3a811a640a383fca1175de53d
Inna <innaingreen@gmail.com>
2022-10-01 17:48:56 +0300

    filters added

--- src/divice-full-info/divice-full-info.component.spec.ts
+++ src/divice-full-info/divice-full-info.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DiviceFullInfoComponent } from './divice-full-info.component';
 
-describe('DiviceFullInfoComponent', () => {
+describe('DeviceFullInfoComponent', () => {
   let component: DiviceFullInfoComponent;
   let fixture: ComponentFixture<DiviceFullInfoComponent>;
 
