a27ca9918f0dfcead5a5603a8a34218b11237ead
Inna <innaingreen@gmail.com>
2022-10-10 20:45:34 +0300

    added Baner(need fixed)

--- src/banner-advertisement/banner-advertisement.component.ts
+++ src/banner-advertisement/banner-advertisement.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerAdvertisementComponent implements OnInit {
 
+  date = new Date();
+  freeShipping = this.date.setDate(this.date.getDate() + 3);
+
+
   constructor() { }
 
   ngOnInit(): void {
